9fe4df9f89f1ef2c118c7da459e2e7b580d26697
kader1303 <kdryere1314@gmail.com>
2022-10-11 20:47:31 -0400

    static 3

--- core/settings.py
+++ core/settings.py
@@ -132,6 +132,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+STATICFILES_DIRS=(BASE_DIR.child('static'),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
