1284b676b693145e9c27a277e0113889f36c833f
kader1303 <kdryere1314@gmail.com>
2022-10-14 19:33:09 -0400

    urld

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
-
+from django.contrib import admin
+from django.conf import settings
+from django.conf.urls import patterns, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -8,3 +10,8 @@ urlpatterns = [
     path('', include('Login.urls')),
     path('administration/', include('Login.urls2')),
 ]
+
+if not settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+    )
