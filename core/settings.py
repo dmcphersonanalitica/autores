78d2621f17059387321725481c23205164083937
kader1303 <kdryere1314@gmail.com>
2022-10-14 09:02:51 -0400

    debug 2

--- core/settings.py
+++ core/settings.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-from operator import truediv
 import dj_database_url
 import django_heroku
 
