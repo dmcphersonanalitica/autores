596ef0c2b6eb136d560d3662037d795ba2cd1498
kader1303 <kdryere1314@gmail.com>
2022-10-14 20:44:09 -0400

    another try

--- core/settings.py
+++ core/settings.py
@@ -126,12 +126,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_URL = '/static/'
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
+
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'staticfiles'),
-)
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+
+STATIC_ROOT = BASE_DIR / 'collected-static'
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
@@ -146,9 +151,12 @@ LOGIN_URL = 'login'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+# MEDIA_URL = '/media/'
+
+# AUTH_USER_MODEL = 'Login.User'
 MEDIA_URL = '/media/'
 
-AUTH_USER_MODEL = 'Login.User'
+MEDIA_ROOT = BASE_DIR / 'collected-media'
 
 # Email
 ANYMAIL = {
