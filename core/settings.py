7b2c72bc5720d6dd0faa0a01d9bbabc3204c4833
kader1303 <kdryere1314@gmail.com>
2022-10-20 12:27:09 -0400

    db

--- core/settings.py
+++ core/settings.py
@@ -88,7 +88,7 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = db.MYSQL
+DATABASES = db.MYSQLONLINE
 
 
 # Password validation
