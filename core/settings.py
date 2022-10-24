2046f01d9c1d405e4d42278b296519aead24b374
kader1303 <kdryere1314@gmail.com>
2022-10-24 19:14:10 -0400

    memcachier 2.0

--- core/settings.py
+++ core/settings.py
@@ -163,50 +163,53 @@ SESSION_SAVE_EVERY_REQUEST = True
 
 SESSION_ENGINE = "django.contrib.sessions.backends.cache"
 
-# servers = os.environ['mc4.c1.nyc1.do.memcachier.com']
-# username = os.environ['EFF17B']
-# password = os.environ['454D1BBD81545DD155FDA674FF73E4EB']
+def get_cache():
+  import os
+  try:
+    servers = os.environ['mc4.c1.nyc1.do.memcachier.com']
+    username = os.environ['EFF17B']
+    password = os.environ['454D1BBD81545DD155FDA674FF73E4EB']
+    
+    return {
+      'default': {
+        'BACKEND': 'django.core.cache.backends.memcached.PyLibMCCache',
+        # TIMEOUT is not the connection timeout! It's the default expiration
+        # timeout that should be applied to keys! Setting it to `None`
+        # disables expiration.
+        'TIMEOUT': None,
+        'LOCATION': servers,
+        'OPTIONS': {
+          'binary': True,
+          'username': username,
+          'password': password,
+          'behaviors': {
+            # Enable faster IO
+            'no_block': True,
+            'tcp_nodelay': True,
+            # Keep connection alive
+            'tcp_keepalive': True,
+            # Timeout settings
+            'connect_timeout': 2000, # ms
+            'send_timeout': 750 * 1000, # us
+            'receive_timeout': 750 * 1000, # us
+            '_poll_timeout': 2000, # ms
+            # Better failover
+            'ketama': True,
+            'remove_failed': 1,
+            'retry_timeout': 2,
+            'dead_timeout': 30,
+          }
+        }
+      }
+    }
+  except:
+    return {
+      'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache'
+      }
+    }
 
-# CACHES = {
-#     'default': {
-#         # Use pylibmc
-#         'BACKEND': 'django.core.cache.backends.memcached.PyLibMCCache',
-
-#         # TIMEOUT is not the connection timeout! It's the default expiration
-#         # timeout that should be applied to keys! Setting it to `None`
-#         # disables expiration.
-#         'TIMEOUT': None,
-
-#         'LOCATION': servers,
-
-#         'OPTIONS': {
-#             # Use binary memcache protocol (needed for authentication)
-#             'binary': True,
-#             'username': username,
-#             'password': password,
-#             'behaviors': {
-#                 # Enable faster IO
-#                 'no_block': True,
-#                 'tcp_nodelay': True,
-
-#                 # Keep connection alive
-#                 'tcp_keepalive': True,
-
-#                 # Timeout settings
-#                 'connect_timeout': 2000, # ms
-#                 'send_timeout': 750 * 1000, # us
-#                 'receive_timeout': 750 * 1000, # us
-#                 '_poll_timeout': 2000, # ms
-
-#                 # Better failover
-#                 'ketama': True,
-#                 'remove_failed': 1,
-#                 'retry_timeout': 2,
-#                 'dead_timeout': 30,
-#             }
-#         }
-#     }
-# }
+CACHES = get_cache()
 
 # CACHES = {
 #     'default': {
