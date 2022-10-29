074e8e475a35ab0e21d58be0c530e18b388f08fd
Marvin Ferrer <marvinfg87@gmail.com>
2022-10-28 23:38:22 -0400

    comentando graficos superusurio dashboard view

--- MECS_Autores/views.py
+++ MECS_Autores/views.py
@@ -203,10 +203,10 @@ class DashboardView(LoginRequiredMixin, TemplateView):
             context['graph_sales_five_year'] = self.graph_sales_five_year()
             context['graph_five_year'] = self.graph_five_year()
             context['graph_sales_month_last_year'] = self.graph_sales_month_last_year()
-        if self.request.user.is_superuser:
-            context['graph_five_year'] = self.graph_five_year()
-            context['graph_sales_month_last_year_general'] = self.graph_sales_month_last_year_general()
-            context['graph_gender_five_year'] = self.graph_gender_five_year()
+        #if self.request.user.is_superuser:
+            #context['graph_five_year'] = self.graph_five_year()
+            #context['graph_sales_month_last_year_general'] = self.graph_sales_month_last_year_general()
+            #context['graph_gender_five_year'] = self.graph_gender_five_year()
         return context
 
 
