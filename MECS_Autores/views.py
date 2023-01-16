86c28efb8fd5c8e112de388541bcd46c3124439b
Marvin Ferrer <marvinfg87@gmail.com>
2023-01-16 11:38:59 -0500

    optimizando lista de ventas v124

--- MECS_Autores/views.py
+++ MECS_Autores/views.py
@@ -204,8 +204,11 @@ class DashboardView(LoginRequiredMixin, TemplateView):
             context['sales'] = 0
             context['total'] = 0
         context['writer'] = self.count_autores()
-        if not self.request.user.is_superuser and hasattr(self.request.user, 'autores') and self.count_ventas() > 0:
-            context['sales_libros'] = self.count_libros_ventas()
+        if not self.request.user.is_superuser and hasattr(self.request.user, 'autores'):
+            if self.count_ventas() > 0:
+                context['sales_libros'] = self.count_libros_ventas()
+            else:
+                context['sales_libros'] = 0
         context['last_sale'] = self.last_ventas()
         context['father'] = 'dashboard'
         if hasattr(self.request.user, 'autores'):
